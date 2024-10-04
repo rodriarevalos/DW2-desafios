02c1d18722f79171e06ac851f3350e13c70a761b
rodrigo <rodrigoarevalos163@gmail.com>
2024-10-04 13:13:03 -0300

    mejore el codigo de js

--- desafio-3/main.js
+++ desafio-3/main.js
@@ -1,6 +1,6 @@
 const NombreProducto = "Naranjas";
 let PrecioUnitario = 25;
-let CantidadDeseada = prompt("¿Cuantas " + NombreProducto + " quiere llevar?");
+let CantidadDeseada = prompt(`¿Cuantas ${NombreProducto} quiere llevar?`);
 
 let CostoTotal = PrecioUnitario * CantidadDeseada;
 
@@ -9,7 +9,7 @@ if (CantidadDeseada >= 5){
     let Descuento = 0.10;
     CostoTotal = CantidadDeseada * PrecioUnitario * (1- Descuento);
 
-    alert(`Se le hace un descuento del 10% por comprar mas de 5 ${NombreProducto}! \n El costo total de su compra es de: ${CostoTotal}`)
+    alert(`Se le realiza un descuento del 10% por comprar mas de 5 ${NombreProducto}! \n\nEl costo total de su compra es de: $${CostoTotal}\nGracias por su compra😊`);
 
 } else{
     CostoTotal = CantidadDeseada * PrecioUnitario;
