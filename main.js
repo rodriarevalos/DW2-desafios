08e54d43c4ae3d8195fb854e2273324c0feda298
rodrigo <rodrigoarevalos163@gmail.com>
2024-10-24 18:51:35 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,30 @@
+//array de productos 
+const producto = ["camiseta","zapatilla", "gorra"];
+const productoPrecio= [15,40,10];
+const productoStock = [5,6,9];
+
+console.log("productos disponibles:");
+for (let i = 0; i<producto.length; i++) {
+    console.log(`${producto[i]} \nprecio:$${productoPrecio[i]} \nstock: ${productoStock[i]}`);
+};
+
+//simulacion de venta
+console.log("simulacion de venta...");
+
+for (let i = 0; i <producto.length; i++){
+    if (productoStock[i] > 0){
+        productoStock[i]--;
+        console.log(`Se vendio una ${producto[i]}`);
+    }
+    else{
+        console.log(`no hay stock de ${producto[i]}`);
+    }
+};
+
+//actualizacin del stock
+console.log("nuevo stock:");
+
+for (let i = 0; i < producto.length; i++) {
+    console.log(`${producto[i]} \nNuevo stock: ${productoStock[i]}`);
+}
+
