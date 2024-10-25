1ea0d808343bbda7a57f480f2af86e1454b8a716
rodrigo <rodrigoarevalos163@gmail.com>
2024-10-25 19:00:25 -0300

    primer commit

--- desafio-5/main.js
+++ desafio-5/main.js
@@ -1,29 +1,13 @@
-//array de productos 
+//array de producto 
 const producto = ["camiseta","zapatilla", "gorra"];
-const productoPrecio= [15,40,10];
-let productoStock = [5,6,9];
 
 console.log("productos disponibles:");
 for (let i = 0; i<producto.length; i++) {
-    console.log(`${producto[i]} \nprecio:$${productoPrecio[i]} \nstock: ${productoStock[i]}`);
+    console.log(`${producto[i]}`);
 };
 
 //simulacion de venta
-console.log("simulacion de venta...");
+console.log("\nsimulacion de venta...");
 
-for (let i = 0; i <producto.length; i++){
-    if (productoStock[i] > 0){
-        productoStock[i]--;
-        console.log(`Se vendio una ${producto[i]}`);
-    }
-    else{
-        console.log(`no hay stock de ${producto[i]}`);
-    }
-};
-
-//actualizacin del stock
-console.log("nuevo stock:");
-
-for (let i = 0; i < producto.length; i++) {
-    console.log(`${producto[i]} \nNuevo stock: ${productoStock[i]}`);
-};
+let productoVendido = producto.pop();
+console.log(`se vendio una ${productoVendido}`);
